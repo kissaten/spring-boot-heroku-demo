fd7ac7898fa3f0931b9664c18f704c0ecbd5a809
Nagaraj.Shanmukhappa <Nagaraj.Shanmukhappa@PCA3802.bi.corp>
2018-02-22 21:16:35 +0530

    person repo

--- src/main/java/com/heroku/demo/PersonRepository.java
+++ src/main/java/com/heroku/demo/PersonRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecordRepository extends JpaRepository<Person, Long> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
