e1ef48794c8e7d3629ed912dc10428eaa8f0ca5b
Nagaraj.Shanmukhappa <Nagaraj.Shanmukhappa@PCA3802.bi.corp>
2018-02-27 16:14:25 +0530

    New Controller test

---
+++ src/main/java/com/heroku/demo/controller/TestController.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2015 Benedikt Ritter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.heroku.demo.controller;
+
+import javax.validation.Valid;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.heroku.demo.*;
+
+@Controller
+@RequestMapping("/test/")
+public class TestController {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public TestController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(ModelMap model) {
+        List<Person> persons = personRepository.findAll();
+        model.addAttribute("persons", persons);
+        model.addAttribute("insertPerson", new Person());
+        return "test";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String insertData(ModelMap model, @ModelAttribute("insertPerson") @Valid Person person,
+                             BindingResult result) {
+        if (!result.hasErrors()) {
+        	personRepository.save(person);
+        }
+        return test(model);
+    }
+}
